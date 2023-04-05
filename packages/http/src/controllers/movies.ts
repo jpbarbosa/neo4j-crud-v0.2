11c35a6ef89f5200eb11489be5b6e45ed811b658
JP Barbosa <jp@jp7.com.br>
2023-04-05 19:57:25 -0300

    HTTP CRUD

--- packages/http/src/controllers/movies.ts
+++ packages/http/src/controllers/movies.ts
@@ -5,26 +5,50 @@ import { NextFunction } from 'express';
 export const moviesController = {
   getAll: async (req, res: AppResponse<Movie[]>, next: NextFunction) => {
     try {
-      const movies = await graph.movies(req.neo4jSession).getAll();
+      const search = req.query.search as string;
+      const movies = await graph.movies(req.neo4jSession).getAll(search);
       res.send(movies);
     } catch (err) {
       next(err);
     }
   },
 
-  getById: async (req, res: AppResponse<string>, next: NextFunction) => {
-    res.send('To be implemented');
+  getById: async (req, res: AppResponse<Movie>, next: NextFunction) => {
+    const id = parseInt(req.params.id);
+    try {
+      const movie = await graph.movies(req.neo4jSession).getById(id);
+      res.send(movie);
+    } catch (err) {
+      next(err);
+    }
   },
 
-  create: async (req, res: AppResponse<string>, next: NextFunction) => {
-    res.send('To be implemented');
+  create: async (req, res: AppResponse<Movie>, next: NextFunction) => {
+    try {
+      const movie = await graph.movies(req.neo4jSession).create(req.body);
+      res.send(movie);
+    } catch (err) {
+      next(err);
+    }
   },
 
-  update: async (req, res: AppResponse<string>, next: NextFunction) => {
-    res.send('To be implemented');
+  update: async (req, res: AppResponse<Movie>, next: NextFunction) => {
+    const id = parseInt(req.params.id);
+    try {
+      const movie = await graph.movies(req.neo4jSession).update(id, req.body);
+      res.send(movie);
+    } catch (err) {
+      next(err);
+    }
   },
 
-  remove: async (req, res: AppResponse<string>, next: NextFunction) => {
-    res.send('To be implemented');
+  remove: async (req, res: AppResponse<Movie>, next: NextFunction) => {
+    const id = parseInt(req.params.id);
+    try {
+      const movie = await graph.movies(req.neo4jSession).remove(id);
+      res.send(movie);
+    } catch (err) {
+      next(err);
+    }
   },
 };
