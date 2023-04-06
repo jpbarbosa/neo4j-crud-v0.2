b71de40de77be2d5d044de7a6489aa2e7ca5abe2
JP Barbosa <jp@jp7.com.br>
2023-04-06 12:08:54 -0300

    Rename HTTP to API

--- packages/api/jest.config.ts
+++ packages/api/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'http',
+  displayName: 'api',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/http',
+  coverageDirectory: '../../coverage/packages/api',
 };
